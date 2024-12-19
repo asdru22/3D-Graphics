d49384e9f9a2119a70d43d5826542e3151e30387
asdru22 <76167257+asdru22@users.noreply.github.com>
2024-12-19 14:59:02 +0100

    Not working, switched to joml functions

--- src/main/java/geom/Cube.java
+++ src/main/java/geom/Cube.java
@@ -3,6 +3,7 @@ package geom;
 import graphics.Camera;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
+import org.joml.Vector4f;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -24,15 +25,15 @@ public class Cube {
 
         Vector3f offset = this.position;
 
-        // Vector3f Coordinates with offset
-        Vector3f v1 = new Vector3f(-scale + offset.x, -scale + offset.y, -scale + offset.z);
-        Vector3f v2 = new Vector3f(scale + offset.x, -scale + offset.y, -scale + offset.z);
-        Vector3f v3 = new Vector3f(scale + offset.x, scale + offset.y, -scale + offset.z);
-        Vector3f v4 = new Vector3f(-scale + offset.x, scale + offset.y, -scale + offset.z);
-        Vector3f v5 = new Vector3f(-scale + offset.x, -scale + offset.y, scale + offset.z);
-        Vector3f v6 = new Vector3f(scale + offset.x, -scale + offset.y, scale + offset.z);
-        Vector3f v7 = new Vector3f(scale + offset.x, scale + offset.y, scale + offset.z);
-        Vector3f v8 = new Vector3f(-scale + offset.x, scale + offset.y, scale + offset.z);
+        // Vector4f Coordinates with offset
+        Vector4f v1 = new Vector4f(-scale + offset.x, -scale + offset.y, -scale + offset.z,1);
+        Vector4f v2 = new Vector4f(scale + offset.x, -scale + offset.y, -scale + offset.z,1);
+        Vector4f v3 = new Vector4f(scale + offset.x, scale + offset.y, -scale + offset.z,1);
+        Vector4f v4 = new Vector4f(-scale + offset.x, scale + offset.y, -scale + offset.z,1);
+        Vector4f v5 = new Vector4f(-scale + offset.x, -scale + offset.y, scale + offset.z,1);
+        Vector4f v6 = new Vector4f(scale + offset.x, -scale + offset.y, scale + offset.z,1);
+        Vector4f v7 = new Vector4f(scale + offset.x, scale + offset.y, scale + offset.z,1);
+        Vector4f v8 = new Vector4f(-scale + offset.x, scale + offset.y, scale + offset.z,1);
 
         // UV Coordinates
         Point uv1 = new Point(0, 0);
@@ -61,10 +62,9 @@ public class Cube {
     }
 
     public void draw(BufferedImage img, double[] zBuffer, Camera cam) {
-        Matrix4f perspective = cam.getPerspective();
-        Matrix4f view = cam.getView();
+        Matrix4f cameraMatrix = cam.getProjectionView();
         for (Triangle t : triangles) {
-            t.draw(perspective, view, img, zBuffer);
+            t.draw(cameraMatrix, img, zBuffer);
         }
     }
 }
--- src/main/java/graphics/Camera.java
+++ src/main/java/graphics/Camera.java
@@ -5,66 +5,36 @@ import org.joml.Vector3f;
 
 public class Camera {
     // Perspective projection parameters
-    private final double fov = 90.0;
-    private double aspectRatio;
-    private final double near = 0.1;
-    private final double far = 1000.0;
+    private final float fov = 90.0f;
+    private float aspectRatio;
+    private final float near = 0.1f;
+    private final float far = 100.0f;
     public double width, height;
 
-    public final Vector3f X_AXIS = new Vector3f(1, 0, 0);
-    public final Vector3f Y_AXIS = new Vector3f(0, 1, 0);
-    public final Vector3f Z_AXIS = new Vector3f(0, 0, 1);
-
     private Vector3f position;
-    private final Vector3f direction = new Vector3f(0, 0, -1);
-    private Matrix4f perspectiveMatrix;
-    private Matrix4f viewMatrix;
+    Vector3f target = new Vector3f(0, 0, 0);
+    Vector3f up = new Vector3f(0, 1, 0);             // "Up" direction is Y-axis
+
+    private Matrix4f projection;
+    private Matrix4f view;
 
     public Camera(double width, double height, Vector3f position) {
         this.position = position;
-        this.aspectRatio = width / height;
+        this.aspectRatio = (float) (width / height);
         this.width = width;
         this.height = height;
         update();
     }
 
     public void update() {
-        perspectiveMatrix = makeProjectionMatrix();
-        viewMatrix = makeViewMatrix();
-
+        projection = new Matrix4f().perspective(fov, aspectRatio, near, far);
+        view = new Matrix4f().lookAt(position, target, up);
+        Matrix4f projectionView = new Matrix4f();
+        projection.mul(view, projectionView);
     }
 
-    public Matrix4f makeProjectionMatrix() {
-
-        float yScale = (float) ((1f / Math.tan(Math.toRadians(fov / 2f))) * aspectRatio);
-        float xScale = (float) (yScale / aspectRatio);
-        var frustrumLength = far - near;
-
-        return new Matrix4f()
-                .m00(xScale)
-                .m11(yScale)
-                .m22((float) -((far + near) / frustrumLength))
-                .m23(-1)
-                .m32((float) -((2 * near * far) / frustrumLength))
-                .m33(0);
-    }
-
-    public Matrix4f makeViewMatrix() {
-        var negatedPosition = new Vector3f(-position.x, -position.y, -position.z);
-
-        return new Matrix4f()
-                .identity()
-                .rotate((float) Math.toRadians(direction.x), X_AXIS)
-                .rotate((float) Math.toRadians(direction.y), Y_AXIS)
-                .rotate((float) Math.toRadians(direction.z), Z_AXIS)
-                .translate(negatedPosition);
-    }
-
-    public Matrix4f getPerspective() {
-        return perspectiveMatrix;
-    }
-    public Matrix4f getView() {
-        return viewMatrix;
+    public Matrix4f getProjectionView() {
+        return projection;
     }
 
     public void moveForward(float amount) {
@@ -103,20 +73,20 @@ public class Camera {
 
     public void addHorizontalRotation(double angle) {
         double radians = Math.toRadians(angle);
-        direction.x += (float) Math.sin(radians);
+        target.x += (float) Math.sin(radians);
 
     }
 
     public void addVerticalRotation(double angle) {
         double radians = Math.toRadians(angle);
-        direction.y += (float) Math.sin(radians);
+        target.y += (float) Math.sin(radians);
         update();
     }
 
     public void resize(int width, int height) {
         this.width = width;
         this.height = height;
-        aspectRatio = (double) width / height;
+        aspectRatio =  (width / height);
         update();
     }
 }
